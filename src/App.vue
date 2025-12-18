f86f9e8e44c84897736da041518ae2efe42ecacb
Aravinda <apranthilaka@outlook.com>
2025-12-18 13:08:10 +0530

    fix

--- src/App.vue
+++ src/App.vue
@@ -7,19 +7,4 @@ import Footer from '@/commonComponents/Sidebar.vue';
     <router-view />
 </template>
 
-<style lang="scss">
-body,
-html {
-    margin: 0;
-    padding: 0;
-    border: 1px solid orange;
-}
-
-body {
-    min-height: 100vh; /* This is the key property */
-    /* Use 100% width over 100vw to avoid horizontal scrollbars */
-    width: 100%;
-    /* Optional: Hide scrollbars if absolutely necessary, but generally not recommended for accessibility */
-    /* overflow: hidden; */
-}
-</style>
+<style lang="scss"></style>
--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -14,12 +14,12 @@ import Sidebar from '@/commonComponents/Sidebar.vue';
 
 <style lang="scss" scoped>
 .layout {
+    height: auto;
     border: 10px solid blue;
     display: grid;
     grid-template-columns: 250px auto;
     /* top menu row fixed at 56px and content row takes remaining height */
     grid-template-rows: 56px 1fr;
     /* fill the viewport so the page container doesn't naturally scroll */
-    height: 100%;
 }
 </style>
